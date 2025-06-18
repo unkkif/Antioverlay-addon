8a3e6b9b9b6ee3fc7d01ddc8d700a39aa9df2b8c
unkkif <samuelvilhelm@gmail.com>
2025-06-19 01:33:33 +0300

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -8,18 +8,18 @@ AddEventHandler("madebyunkki::ban", function(reason)
     if isRecording[src] then return end
     isRecording[src] = true
 
-    if Config.recordplayer then
-        exports[Config.FiveguardName]:recordPlayerScreen(src, Config.recordtime, function(success)
+    if Config.Recordplayer then
+        exports[Config.Fiveguardname]:recordPlayerScreen(src, Config.Recordtime, function(success)
             if success then
                 print("Screenshot success for player: " .. src)
-                exports[Config.FiveguardName]:fg_BanPlayer(src, banReason, true)
+                exports[Config.Fiveguardname]:fg_BanPlayer(src, banReason, true)
             else
                 print("Screenshot failed for player: " .. src)
             end
             isRecording[src] = false
         end, Config.Webhook)
     else
-        exports[Config.FiveguardName]:fg_BanPlayer(src, banReason, true)
+        exports[Config.Fiveguardname]:fg_BanPlayer(src, banReason, true)
         isRecording[src] = false
     end
 end)
