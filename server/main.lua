a30ba5ff7b47679afe917a4c72cfc298cd8a8e6d
unkkif <samuelvilhelm@gmail.com>
2025-06-19 19:41:51 +0300

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,12 +1,53 @@
 local isRecording = {}
 local banReason = Locales.OverlayDetected
 
+RegisterServerEvent("madebyunkki:checkWhitelist", function()
+    local src = source
+    local identifiers = GetPlayerIdentifiers(src)
+    local steamIdentifier
+
+    for _, id in pairs(identifiers) do
+        if string.sub(id, 1, 6) == "steam:" then
+            steamIdentifier = id
+            break
+        end
+    end
+
+    local isWhitelisted = false
+    if steamIdentifier then
+        for _, v in pairs(Shared.Whitelist) do
+            if steamIdentifier == v then
+                isWhitelisted = true
+                break
+            end
+        end
+    end
+
+    TriggerClientEvent("madebyunkki:setWhitelistStatus", src, isWhitelisted)
+end)
 
 RegisterServerEvent("madebyunkki::ban")
 AddEventHandler("madebyunkki::ban", function(reason)
     local src = source
     if isRecording[src] then return end
     isRecording[src] = true
+
+    local identifiers = GetPlayerIdentifiers(src)
+    local steamIdentifier
+
+    for _, id in pairs(identifiers) do
+        if string.sub(id, 1, 6) == "steam:" then
+            steamIdentifier = id
+            break
+        end
+    end
+
+    for _, v in pairs(Shared.Whitelist) do
+        if steamIdentifier == v then
+            isRecording[src] = false
+            return
+        end
+    end
 
     if Config.Recordplayer then
         exports[Config.Fiveguardname]:recordPlayerScreen(src, Config.Recordtime, function(success)
