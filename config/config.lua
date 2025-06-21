4da2ee2c9dfc4db1c49634d4d6458f50ef3b047c
unkkif <samuelvilhelm@gmail.com>
2025-06-22 02:58:46 +0300

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -6,27 +6,31 @@ Config.Recordplayer = true -- true or false
 Config.Webhook = "your-webhook-here" -- The video will be sent to this URL, can be useful for review.
 Config.Recordtime = 1500 -- 1500 = 1.5sec
 
-
-
-
 -- Overlay Setting
 Config.MaxAttempts = 5
+
 -- Maximum number of allowed mouse movement attempts during the overlay check.
 -- If the player moves the mouse too many times, they will be banned.
 
-Config.BaseDuration = 5000 -- A value set too high can result in false bans. I recommend keeping it below 5000ms.
+Config.BaseDuration = 4000 
+
+-- A value set too high can result in false bans. I recommend keeping it below 5000ms.
 -- The base duration of the overlay protection in milliseconds (5000 ms = 5 seconds).
 -- This defines how long the overlay detection remains active before it automatically stops.
 
 Config.AdditionalDuration = 5000
+
 -- Extra time in milliseconds added to the overlay protection duration
 -- for each detected mouse movement during the check.
 -- This prolongs the overlay detection period whenever mouse activity is detected.
 
-Config.OverlayKey = 121
--- The key code that triggers the overlay check when pressed.
--- 121 corresponds to the INSERT key.
--- You can change this to any other key code as needed.
+Config.OverlayKey = {121}
+  
+-- You can add multiple keys here, e.g. {36, 121}
+-- 36 is Right Shift (used on Susano)
+-- 121 is the INSERT key (used on tzx, redengine, eulen and tz)
+-- This config defines which key(s) trigger the overlay check when pressed.
+-- Feel free to change or add key codes as needed.
 
 
 -- BTW THIS CANT BE DUMPED!
